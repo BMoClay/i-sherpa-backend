c95753c2d6cb8ae15cffbd0bb0f1c723158f5ecb
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-25 12:47:49 -0500

    Ben and I worked on controllers

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,2 +1,11 @@
 class GuidesController < ApplicationController
+    def index
+        guides = Guide.all
+        render json: guides
+    end
+
+    def show
+        guide = Guide.find(params[:id])
+        render json: guide
+    end
 end

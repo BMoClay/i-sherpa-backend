9a5f46e41f7b75d18f8b00ffc31cc4a19da0a39e
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-26 16:08:37 -0500

    Ben and I finished MVP

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -9,4 +9,27 @@ class GuidesController < ApplicationController
         guide = Guide.find(params[:id])
         render json: guide
     end
+
+    def create
+        guide = Guide.create(guide_params)
+        render json: guide
+    end
+    
+    def update
+        guide = Guide.find(params[:id])
+        guide.update(guide_params)
+        render json: guide
+    end
+
+    def destroy
+        guide = Guide.find(params[:id])
+        guide.destroy
+        render json: guide
+    end
+
+    private 
+
+    def guide_params
+        params.permit(:user_id, :title, :category, :content, :img_url)   
+    end
 end
